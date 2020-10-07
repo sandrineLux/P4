ab2113a9cd6bb42a122699964ad8b01adf85d4a8
sandrineLux <68812505+sandrineLux@users.noreply.github.com>
2020-10-07 16:39:48 +0200

    Update Flights_Delays.py

--- Flights_Delays.py
+++ Flights_Delays.py
@@ -37,7 +37,7 @@ def prediction():
         y = df.ARR_DELAY
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) 
         
-        ridge = sklearn.linear_model.Ridge()
+        ridge = Ridge()
         ridge.set_params(alpha=48.24)
         ridge.fit(X_train, y_train)
 
