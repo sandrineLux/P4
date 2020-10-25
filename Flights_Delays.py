c72d9f8f44f99b98547125ca59698a7ba89ee687
sandrineLux <68812505+sandrineLux@users.noreply.github.com>
2020-10-25 19:30:22 +0100

    Update Flights_Delays.py

--- Flights_Delays.py
+++ Flights_Delays.py
@@ -34,7 +34,7 @@ def prediction():
         df = pd.get_dummies(df, columns = ['DEST'])
         df = pd.get_dummies(df, columns = ['ORIGIN'])
         
-        X = df.drop('ARR_DELAY',axis=1)
+        X = df.drop('DEP_DELAY',axis=1)
         y = df.DEP_DELAY
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) 
         
